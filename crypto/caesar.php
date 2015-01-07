784c1bb0ec36828fc4b9579e464d5bf2efa6e6e4
keithieopia <timothy@keithieopia.com>
2015-01-07 13:03:56 -0500

    Added license information

--- crypto/caesar.php
+++ crypto/caesar.php
@@ -1,60 +1,78 @@
 <?php
-/*
+<?php
+/**
+ * caesar.crypt.php
+ *
  * PHP implementation of a shift cipher
- * 
- * Encrypts and decrypts text based on a alphabetic shift/rotation cipher 
- * such as the Caesar and ROT13 ciphers. This version is compatible with 
- * the generic pen and paper method: the text is NOT transformed to ASCII 
+ *
+ * Encrypts and decrypts text based on a alphabetic shift/rotation cipher
+ * such as the Caesar and ROT13 ciphers. This version is compatible with
+ * the generic pen and paper method: the text is NOT transformed to ASCII
  * before the rotation (non A-Z characters are ignored).
- * 
- * @author     Timothy Keith <timothy@keithieopia.com>
- * @copyright  2011 Timothy Keith
- * @license    http://www.gnu.org/copyleft/gpl.html GNU/GPL
- * @link       https://github.com/keithieopia/snippets
- */ 
+ *
+ * Copyright (C) 2011 Timothy Keith <timothy@keithieopia.com>
+ *
+ * LICENSE: This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author      Timothy Keith <timothy@keithieopia.com>
+ * @copyright   Timothy Keith 2011
+ * @license     https://www.gnu.org/licenses/gpl.html
+ * @link        http://keithieopia.com
+ */
 
-if (isset($_POST['message'])){	
-	$message  = $_POST['message'];
-	$rotation = $_POST['rotation'];
-	
-	for($i=0; $i < strlen($message); $i++){
+if (isset($_POST['message'])){
+    $message  = $_POST['message'];
+    $rotation = $_POST['rotation'];
 
-		$letter   = strtoupper($message{$i});
+    for($i=0; $i < strlen($message); $i++){
 
-		// If the character is a letter and not a space, period, comma, etc...
-		if (preg_match("/^[A-Z]$/", $letter)) { 
-		
-			// Turn the letter into it's numeric position in the alphabet
-			// eg: A = 1, B = 2, etc....
-			$position = ((int) ord($letter) - 64);
-			
-			if (isset($_POST['encrypt'])){
+        $letter   = strtoupper($message{$i});
 
-				if (($position + $rotation) > 26) {
-					$position = ($position + $rotation) - 26;
-				}
-				else {
-					$position = $position + $rotation;
-				}
-			}
-			
-			if (isset($_POST['decrypt'])){
-				// We add 26 to prevent a negative
-				$position = (26 + ($position - $rotation)) % 26;
-			}
-			
-			// Turn position back into a letter
-			$letter = chr($position + 64); 
-			
-			// Lowercase letter if it was in the message
-			if (ctype_lower($_POST['message']{$i})){
-				$letter = strtolower($letter);
-			}
-			
-			// Set the letter in the message
-			$_POST['message']{$i} = $letter;
-		}
-	}
+        // If the character is a letter and not a space, period, comma, etc...
+        if (preg_match("/^[A-Z]$/", $letter)) {
+
+            // Turn the letter into it's numeric position in the alphabet
+            // eg: A = 1, B = 2, etc....
+            $position = ((int) ord($letter) - 64);
+
+            if (isset($_POST['encrypt'])){
+
+                if (($position + $rotation) > 26) {
+                    $position = ($position + $rotation) - 26;
+                }
+                else {
+                    $position = $position + $rotation;
+                }
+            }
+
+            if (isset($_POST['decrypt'])){
+                // We add 26 to prevent a negative
+                $position = (26 + ($position - $rotation)) % 26;
+            }
+
+            // Turn position back into a letter
+            $letter = chr($position + 64);
+
+            // Lowercase letter if it was in the message
+            if (ctype_lower($_POST['message']{$i})){
+                $letter = strtolower($letter);
+            }
+
+            // Set the letter in the message
+            $_POST['message']{$i} = $letter;
+        }
+    }
 }
 
 ?>
@@ -62,38 +80,124 @@ if (isset($_POST['message'])){
 <h1>Hello Mrs. Keith's &amp; Mrs. Smith's Classes!</h1>
 
 <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-	<textarea name="message" cols="40" rows="5"><?php if (isset($_POST['message'])) echo $_POST['message']; ?></textarea><br />
+    <textarea name="message" cols="40" rows="5"><?php if (isset($_POST['message'])) echo $_POST['message']; ?></textarea><br />
 
 Rotation:
 
-	<select name="rotation">
-		<option value="1">1  (A => B)</option>
-		<option value="2">2  (A => C)</option>
-		<option value="3">3  (A => D)</option>
-		<option value="4">4  (A => E)</option>
-		<option value="5">5  (A => F)</option>
-		<option value="6">6  (A => G)</option>
-		<option value="7">7  (A => H)</option>
-		<option value="8">8  (A => I)</option>
-		<option value="9">9  (A => J)</option>
-		<option value="10">10 (A => K)</option>
-		<option value="11">11 (A => L)</option>
-		<option value="12">12 (A => M)</option>
-		<option value="13">13 (A => N)</option>
-		<option value="14">14 (A => O)</option>
-		<option value="15">15 (A => P)</option>
-		<option value="16">16 (A => Q)</option>
-		<option value="17">17 (A => R)</option>
-		<option value="18">18 (A => S)</option>
-		<option value="19">19 (A => T)</option>
-		<option value="20">20 (A => U)</option>
-		<option value="21">21 (A => V)</option>
-		<option value="22">22 (A => W)</option>
-		<option value="23">23 (A => X)</option>
-		<option value="24">24 (A => Y)</option>
-		<option value="25">25 (A => Z)</option>
-	</select><br />
+    <select name="rotation">
+        <option value="1">1  (A => B)</option>
+        <option value="2">2  (A => C)</option>
+        <option value="3">3  (A => D)</option>
+        <option value="4">4  (A => E)</option>
+        <option value="5">5  (A => F)</option>
+        <option value="6">6  (A => G)</option>
+        <option value="7">7  (A => H)</option>
+        <option value="8">8  (A => I)</option>
+        <option value="9">9  (A => J)</option>
+        <option value="10">10 (A => K)</option>
+        <option value="11">11 (A => L)</option>
+        <option value="12">12 (A => M)</option>
+        <option value="13">13 (A => N)</option>
+        <option value="14">14 (A => O)</option>
+        <option value="15">15 (A => P)</option>
+        <option value="16">16 (A => Q)</option>
+        <option value="17">17 (A => R)</option>
+        <option value="18">18 (A => S)</option>
+        <option value="19">19 (A => T)</option>
+        <option value="20">20 (A => U)</option>
+        <option value="21">21 (A => V)</option>
+        <option value="22">22 (A => W)</option>
+        <option value="23">23 (A => X)</option>
+        <option value="24">24 (A => Y)</option>
+        <option value="25">25 (A => Z)</option>
+    </select><br />
 
-	<input name="encrypt" type="submit" value="Encrypt" />
-	<input name="decrypt" type="submit" value="Decrypt" />
+    <input name="encrypt" type="submit" value="Encrypt" />
+    <input name="decrypt" type="submit" value="Decrypt" />
+</form>
+
+
+if (isset($_POST['message'])){
+    $message  = $_POST['message'];
+    $rotation = $_POST['rotation'];
+
+    for($i=0; $i < strlen($message); $i++){
+
+        $letter   = strtoupper($message{$i});
+
+        // If the character is a letter and not a space, period, comma, etc...
+        if (preg_match("/^[A-Z]$/", $letter)) {
+
+            // Turn the letter into it's numeric position in the alphabet
+            // eg: A = 1, B = 2, etc....
+            $position = ((int) ord($letter) - 64);
+
+            if (isset($_POST['encrypt'])){
+
+                if (($position + $rotation) > 26) {
+                    $position = ($position + $rotation) - 26;
+                }
+                else {
+                    $position = $position + $rotation;
+                }
+            }
+
+            if (isset($_POST['decrypt'])){
+                // We add 26 to prevent a negative
+                $position = (26 + ($position - $rotation)) % 26;
+            }
+
+            // Turn position back into a letter
+            $letter = chr($position + 64);
+
+            // Lowercase letter if it was in the message
+            if (ctype_lower($_POST['message']{$i})){
+                $letter = strtolower($letter);
+            }
+
+            // Set the letter in the message
+            $_POST['message']{$i} = $letter;
+        }
+    }
+}
+
+?>
+
+<h1>Hello Mrs. Keith's &amp; Mrs. Smith's Classes!</h1>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+    <textarea name="message" cols="40" rows="5"><?php if (isset($_POST['message'])) echo $_POST['message']; ?></textarea><br />
+
+Rotation:
+
+    <select name="rotation">
+        <option value="1">1  (A => B)</option>
+        <option value="2">2  (A => C)</option>
+        <option value="3">3  (A => D)</option>
+        <option value="4">4  (A => E)</option>
+        <option value="5">5  (A => F)</option>
+        <option value="6">6  (A => G)</option>
+        <option value="7">7  (A => H)</option>
+        <option value="8">8  (A => I)</option>
+        <option value="9">9  (A => J)</option>
+        <option value="10">10 (A => K)</option>
+        <option value="11">11 (A => L)</option>
+        <option value="12">12 (A => M)</option>
+        <option value="13">13 (A => N)</option>
+        <option value="14">14 (A => O)</option>
+        <option value="15">15 (A => P)</option>
+        <option value="16">16 (A => Q)</option>
+        <option value="17">17 (A => R)</option>
+        <option value="18">18 (A => S)</option>
+        <option value="19">19 (A => T)</option>
+        <option value="20">20 (A => U)</option>
+        <option value="21">21 (A => V)</option>
+        <option value="22">22 (A => W)</option>
+        <option value="23">23 (A => X)</option>
+        <option value="24">24 (A => Y)</option>
+        <option value="25">25 (A => Z)</option>
+    </select><br />
+
+    <input name="encrypt" type="submit" value="Encrypt" />
+    <input name="decrypt" type="submit" value="Decrypt" />
 </form>
