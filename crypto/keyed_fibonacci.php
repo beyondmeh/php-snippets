2106bd22c10fdc6f4e1dad4f745aab66c54b6742
keithieopia <timothy@keithieopia.com>
2015-01-07 13:06:15 -0500

    Added license

--- crypto/keyed_fibonacci.php
+++ crypto/keyed_fibonacci.php
@@ -1,102 +1,119 @@
 <?php
-/*
+/**
+ * keyed_fibonacci.php
+ *
  * A Polyalphabetic Cipher using a lagged fibonacci generator.
- * 
+ *
  * Encrypts and decrypts text given a certain password. The password
- * is used to seed a lagged fibonacci generator which in turn is used to 
+ * is used to seed a lagged fibonacci generator which in turn is used to
  * generate a number for each letter of the message. That number is then
  * used to rotate each letter of the message.
- * 
+ *
  * Excluding the common issues with passwords and complexity, the cipher
- * itself is relatively secure for these types of ciphers. The lagged 
- * fibonacci generator effectively acts as pseudorandom number generator 
- * which in turn defeats frequency analysis. 
- * 
- * @author     Timothy Keith <timothy@keithieopia.com>
- * @copyright  2011 Timothy Keith
- * @license    http://www.gnu.org/copyleft/gpl.html GNU/GPL
- * @link       https://github.com/keithieopia/snippets
- */ 
- 
-if (isset($_POST['message'])){	
-	$message  = $_POST['message'];
-	$password = strtoupper($_POST['password']);
-	
-	$password = preg_replace('/[^A-Z]/', '', $password);
+ * itself is relatively secure for these types of ciphers. The lagged
+ * fibonacci generator effectively acts as pseudorandom number generator
+ * which in turn defeats frequency analysis.
+ *
+ * Copyright (C) 2011 Timothy Keith <timothy@keithieopia.com>
+ *
+ * LICENSE: This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author      Timothy Keith <timothy@keithieopia.com>
+ * @copyright   Timothy Keith 2011
+ * @license     https://www.gnu.org/licenses/gpl.html
+ * @link        http://keithieopia.com
+ */
 
-	// Generate the seed
-	$seed = '';
-	for ($i=0; $i < strlen($password); $i++){
-		$seed .= ord($password{$i}) - 65;
-	}
+if (isset($_POST['message'])){
+    $message  = $_POST['message'];
+    $password = strtoupper($_POST['password']);
 
-	
-	/* Made a seed using a fibonacci like algorithm... 
-	 * Add the last 2 letter's positions of the password 
-	 * and mod it by 26 to get a letter.
-	 */ 
-	$password = '';
+    $password = preg_replace('/[^A-Z]/', '', $password);
 
-	$n1 = 1;
-	$n2 = $seed;
-	for ($i=0; $i < strlen($message); $i++){
-		$n3 = ($n1 + $n2) % 26;
-		
-		$password .= chr($n3 + 65);
-		
-		$n1 = $n2;
-		$n2 = $n3;
-	}	
-	
-	/* Seperate counter for password, so spaces and commas don't change
-	 * the encryption.
-	 */ 
+    // Generate the seed
+    $seed = '';
+    for ($i=0; $i < strlen($password); $i++){
+        $seed .= ord($password{$i}) - 65;
+    }
 
-	$pass_i = 0; 
 
-	for($i=0; $i < strlen($message); $i++){
-		$m_letter = strtoupper($message{$i});
-		$p_letter = strtoupper($password{$pass_i});
-		
-		$m_pos = ord($m_letter) - 65;
-		$p_pos = ord($p_letter) - 65;
+    /* Made a seed using a fibonacci like algorithm...
+     * Add the last 2 letter's positions of the password
+     * and mod it by 26 to get a letter.
+     */
+    $password = '';
 
-		// If the character is a letter and not a space, period, comma, etc...
-		if (preg_match("/^[A-Z]$/", $m_letter)) { 
-			
-			if (isset($_POST['encrypt'])){
-				$pos    = ($m_pos + $p_pos) % 26;
-				$letter = chr($pos + 65);
-			}
+    $n1 = 1;
+    $n2 = $seed;
+    for ($i=0; $i < strlen($message); $i++){
+        $n3 = ($n1 + $n2) % 26;
 
-			if (isset($_POST['decrypt'])){
-				// We add 26 to prevent a negative
-				$pos    = (26 + ($m_pos - $p_pos)) % 26;
-				$letter = chr($pos + 65);
-			}
+        $password .= chr($n3 + 65);
 
-			// Lowercase letter if it was in the message
-			if (ctype_lower($_POST['message']{$i})){
-				$letter = strtolower($letter);
-			}
+        $n1 = $n2;
+        $n2 = $n3;
+    }
 
-			$_POST['message']{$i} = $letter;
-			$pass_i++;
-		} 
-	}
+    /* Seperate counter for password, so spaces and commas don't change
+     * the encryption.
+     */
+
+    $pass_i = 0;
+
+    for($i=0; $i < strlen($message); $i++){
+        $m_letter = strtoupper($message{$i});
+        $p_letter = strtoupper($password{$pass_i});
+
+        $m_pos = ord($m_letter) - 65;
+        $p_pos = ord($p_letter) - 65;
+
+        // If the character is a letter and not a space, period, comma, etc...
+        if (preg_match("/^[A-Z]$/", $m_letter)) {
+
+            if (isset($_POST['encrypt'])){
+                $pos    = ($m_pos + $p_pos) % 26;
+                $letter = chr($pos + 65);
+            }
+
+            if (isset($_POST['decrypt'])){
+                // We add 26 to prevent a negative
+                $pos    = (26 + ($m_pos - $p_pos)) % 26;
+                $letter = chr($pos + 65);
+            }
+
+            // Lowercase letter if it was in the message
+            if (ctype_lower($_POST['message']{$i})){
+                $letter = strtolower($letter);
+            }
+
+            $_POST['message']{$i} = $letter;
+            $pass_i++;
+        }
+    }
 }
 
 ?>
 
 <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-	<textarea name="message" cols="40" rows="5"><?php if (isset($_POST['message'])) echo $_POST['message']; ?></textarea><br />
+    <textarea name="message" cols="40" rows="5"><?php if (isset($_POST['message'])) echo $_POST['message']; ?></textarea><br />
 
-	Password:
-	<input name="password" type="text" value="<?php if (isset($_POST['password'])) echo $_POST['password']; ?>"><br />
-	
-	Fibonacci: 
-	<input type="text" name="fibonacci" value="<?php if (isset($password)) echo $password; ?>" disabled="disabled" /><br />
-	
-	<input name="encrypt" type="submit" value="Encrypt" />
-	<input name="decrypt" type="submit" value="Decrypt" />
+    Password:
+    <input name="password" type="text" value="<?php if (isset($_POST['password'])) echo $_POST['password']; ?>"><br />
+
+    Fibonacci:
+    <input type="text" name="fibonacci" value="<?php if (isset($password)) echo $password; ?>" disabled="disabled" /><br />
+
+    <input name="encrypt" type="submit" value="Encrypt" />
+    <input name="decrypt" type="submit" value="Decrypt" />
 </form>
